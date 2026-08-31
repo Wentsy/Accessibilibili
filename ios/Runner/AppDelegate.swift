4d02668dbeab1695e7f469cadbc221399e2d9532
Wentsy <121807470+Wentsy@users.noreply.github.com>
2026-09-01 03:45:21 +0800

    feat: forward VoiceOver Magic Tap to Flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private var accessibilityChannel: FlutterMethodChannel?
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -13,5 +14,19 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    accessibilityChannel = FlutterMethodChannel(
+      name: "accessibilibili/accessibility",
+      binaryMessenger: engineBridge.applicationRegistrar.messenger()
+    )
+  }
+
+  override func accessibilityPerformMagicTap() -> Bool {
+    guard let channel = accessibilityChannel else {
+      return super.accessibilityPerformMagicTap()
+    }
+
+    channel.invokeMethod("magicTap", arguments: nil)
+    return true
   }
 }
