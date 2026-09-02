ebba6350fe58694d2416d737dc6718e1c3f6ea4e
Wentsy <121807470+Wentsy@users.noreply.github.com>
2026-09-03 06:20:57 +0800

    fix: make favorite topics VoiceOver complete items

--- lib/pages/fav/topic/view.dart
+++ lib/pages/fav/topic/view.dart
@@ -1,3 +1,5 @@
+import 'package:PiliPlus/common/a11y/a11y_focus_scroll.dart';
+import 'package:PiliPlus/common/a11y/voiceover_paged_scroll.dart';
 import 'package:PiliPlus/common/style.dart';
 import 'package:PiliPlus/common/widgets/dialog/dialog.dart';
 import 'package:PiliPlus/common/widgets/flutter/refresh_indicator.dart';
@@ -9,6 +11,7 @@ import 'package:PiliPlus/models_new/fav/fav_topic/topic_item.dart';
 import 'package:PiliPlus/pages/fav/topic/controller.dart';
 import 'package:PiliPlus/utils/grid.dart';
 import 'package:PiliPlus/utils/platform_utils.dart';
+import 'package:flutter/semantics.dart';
 import 'package:get/get.dart';
 import 'package:material_ui/material_ui.dart'
     hide SliverGridDelegateWithMaxCrossAxisExtent;
@@ -33,22 +36,25 @@ class _FavTopicPageState extends State<FavTopicPage>
     final ThemeData theme = Theme.of(context);
     return refreshIndicator(
       onRefresh: _controller.onRefresh,
-      child: CustomScrollView(
+      child: VoiceOverPagedScroll(
         controller: _controller.scrollController,
-        physics: const AlwaysScrollableScrollPhysics(),
-        slivers: [
-          SliverPadding(
-            padding: EdgeInsets.only(
-              left: Style.safeSpace,
-              right: Style.safeSpace,
-              top: Style.safeSpace,
-              bottom: MediaQuery.viewPaddingOf(context).bottom + 100,
+        child: CustomScrollView(
+          controller: _controller.scrollController,
+          physics: const AlwaysScrollableScrollPhysics(),
+          slivers: [
+            SliverPadding(
+              padding: EdgeInsets.only(
+                left: Style.safeSpace,
+                right: Style.safeSpace,
+                top: Style.safeSpace,
+                bottom: MediaQuery.viewPaddingOf(context).bottom + 100,
+              ),
+              sliver: Obx(
+                () => _buildBody(theme, _controller.loadingState.value),
+              ),
             ),
-            sliver: Obx(
-              () => _buildBody(theme, _controller.loadingState.value),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -87,37 +93,55 @@ class _FavTopicPageState extends State<FavTopicPage>
                     onConfirm: () => _controller.onRemove(index, item.id!),
                   );
 
-                  return Material(
-                    color: theme.colorScheme.onInverseSurface,
-                    borderRadius: const BorderRadius.all(Radius.circular(6)),
-                    child: InkWell(
-                      onTap: () => Get.toNamed(
-                        '/dynTopic',
-                        parameters: {
-                          'id': item.id!.toString(),
-                          'name': item.name!,
-                        },
-                      ),
-                      onLongPress: onLongPress,
-                      onSecondaryTap: PlatformUtils.isMobile
-                          ? null
-                          : onLongPress,
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(6),
-                      ),
-                      child: Container(
-                        alignment: Alignment.centerLeft,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 11,
-                          vertical: 5,
+                  void openTopic() => Get.toNamed(
+                    '/dynTopic',
+                    parameters: {
+                      'id': item.id!.toString(),
+                      'name': item.name!,
+                    },
+                  );
+
+                  return Semantics(
+                    container: true,
+                    explicitChildNodes: false,
+                    excludeSemantics: true,
+                    button: true,
+                    label: '話題 ${item.name}',
+                    hint: '點兩下開啟話題。上下滑有取消收藏',
+                    onTap: openTopic,
+                    onDidGainAccessibilityFocus: () =>
+                        a11yEnsureVisible(context),
+                    customSemanticsActions:
+                        <CustomSemanticsAction, VoidCallback>{
+                      const CustomSemanticsAction(label: '取消收藏'):
+                          onLongPress,
+                    },
+                    child: Material(
+                      color: theme.colorScheme.onInverseSurface,
+                      borderRadius: const BorderRadius.all(Radius.circular(6)),
+                      child: InkWell(
+                        onTap: openTopic,
+                        onLongPress: onLongPress,
+                        onSecondaryTap: PlatformUtils.isMobile
+                            ? null
+                            : onLongPress,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(6),
                         ),
-                        child: Text(
-                          '# ${item.name}',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: theme.colorScheme.onSurfaceVariant,
+                        child: Container(
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 11,
+                            vertical: 5,
+                          ),
+                          child: Text(
+                            '# ${item.name}',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: theme.colorScheme.onSurfaceVariant,
+                            ),
                           ),
                         ),
                       ),
