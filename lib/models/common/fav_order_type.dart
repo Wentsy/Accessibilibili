3980681244765efe2d19036db683eae9330cecf5
Wentsy <121807470+Wentsy@users.noreply.github.com>
2026-09-03 07:16:01 +0800

    feat: add ascending favorite video sort options

--- lib/models/common/fav_order_type.dart
+++ lib/models/common/fav_order_type.dart
@@ -1,10 +1,15 @@
 enum FavOrderType {
-  mtime('最近收藏'),
-  view('最多播放'),
-  pubtime('最近投稿'),
+  mtime('最近收藏', 'mtime', true),
+  mtimeAsc('最早收藏', 'mtime', false),
+  view('最多播放', 'view', true),
+  viewAsc('最少播放', 'view', false),
+  pubtime('最近投稿', 'pubtime', true),
+  pubtimeAsc('最早投稿', 'pubtime', false),
   ;
 
   final String label;
+  final String apiName;
+  final bool descending;
 
-  const FavOrderType(this.label);
+  const FavOrderType(this.label, this.apiName, this.descending);
 }
