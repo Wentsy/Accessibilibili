e017eff4033fe76844eb7e57455b2f21be1239e2
Wentsy <121807470+Wentsy@users.noreply.github.com>
2026-09-06 08:10:58 +0800

    fix(a11y): correct nested reply dislike feedback

--- lib/common/a11y/reply_semantics.dart
+++ lib/common/a11y/reply_semantics.dart
@@ -124,7 +124,7 @@ class _ReplyA11ySemanticsState extends State<ReplyA11ySemantics> {
       a11yActionFeedback(message: message);
       (context as Element).markNeedsBuild();
     } else {
-      final message = isDisliked ? '取消踩失败' : '点赞失败，需要登录';
+      final message = isDisliked ? '取消踩失败' : '点踩失败，需要登录';
       SmartDialog.showToast(message);
       a11yActionFeedback(message: message);
     }
